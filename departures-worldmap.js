8fc1a2de3aaf40808d6e625edf3f670fec324238
n669dgpsjp-a11y <n669dgpsjp@privaterelay.appleid.com>
2026-09-05 22:34:20 +0100

    Lighten departures map land colour

--- departures-worldmap.js
+++ departures-worldmap.js
@@ -7,11 +7,11 @@ window.mapSvg=function(f){
     <defs>
       <clipPath id="worldClip"><rect x="0" y="0" width="360" height="151"/></clipPath>
       <filter id="warmLand" x="-5%" y="-5%" width="110%" height="110%" color-interpolation-filters="sRGB">
-        <feColorMatrix type="matrix" values="0.18 0.18 0.18 0 0.46  0.17 0.17 0.17 0 0.44  0.12 0.12 0.12 0 0.36  0 0 0 1 0"/>
+        <feColorMatrix type="matrix" values="0.10 0.10 0.10 0 0.68  0.10 0.10 0.10 0 0.66  0.08 0.08 0.08 0 0.57  0 0 0 1 0"/>
       </filter>
     </defs>
     <g clip-path="url(#worldClip)">
-      <image href="https://commons.wikimedia.org/wiki/Special:FilePath/BlankMap-Equirectangular.svg" x="0" y="0" width="360" height="180" preserveAspectRatio="none" opacity="0.72" filter="url(#warmLand)"/>
+      <image href="https://commons.wikimedia.org/wiki/Special:FilePath/BlankMap-Equirectangular.svg" x="0" y="0" width="360" height="180" preserveAspectRatio="none" opacity="0.62" filter="url(#warmLand)"/>
     </g>
     <path class="map-route" d="M${x1.toFixed(1)} ${y1.toFixed(1)} Q${cx.toFixed(1)} ${cy.toFixed(1)} ${x2.toFixed(1)} ${y2.toFixed(1)}"/>
     <circle class="map-airport-dot" cx="${x1.toFixed(1)}" cy="${y1.toFixed(1)}" r="5"/>
